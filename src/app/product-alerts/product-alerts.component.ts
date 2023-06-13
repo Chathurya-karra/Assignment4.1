da39d51dfbf6806cde68f117629b7c3b6862de2f
chan <Karrachathurya1998@gmail.com>
2023-06-13 10:25:33 +0300

    first commit

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
